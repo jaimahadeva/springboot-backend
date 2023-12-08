90f7152bc2a035e57ebebea9ae8af28559889eef
mahadeva <mahadevb909@gmail.com>
2023-12-08 21:10:46 +0530

    cors try 8

--- src/main/java/com/example/prep/PreptalksbackendApplication.java
+++ src/main/java/com/example/prep/PreptalksbackendApplication.java
@@ -11,6 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -23,21 +26,30 @@ public class PreptalksbackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PreptalksbackendApplication.class, args);
 	}
-	@Bean
-	public org.springframework.web.filter.CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://jaimahadeva.github.io",  "http://localhost:4200","https://jaimahadeva.github.io/", "https://jaimahadeva.github.io/jsr/","https://prepshopbackend.onrender.com"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
+		@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
+	// @Bean
+	// public org.springframework.web.filter.CorsFilter corsFilter() {
+	// 	CorsConfiguration corsConfiguration = new CorsConfiguration();
+	// 	corsConfiguration.setAllowCredentials(true);
+	// 	corsConfiguration.setAllowedOrigins(Arrays.asList("https://jaimahadeva.github.io",  "http://localhost:4200","https://jaimahadeva.github.io/", "https://jaimahadeva.github.io/jsr/","https://prepshopbackend.onrender.com"));
+	// 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+	// 			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+	// 			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+	// 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+	// 			"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+	// 	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	// 	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+	// 	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+	// 	return new CorsFilter(urlBasedCorsConfigurationSource);
+	// }
 //	@Bean
 //	   public Docket productApi() {
 //	      return new Docket(DocumentationType.SWAGGER_2).select()
